9f5e6a7de513718e01a3f5cc5ab2314294e67ee5
sotty <dsotty@gmail.com>
2014-01-03 12:25:37 +0100

    [DROOLS-392] KIE APIs do not allow to configure the Belief System type

--- drools-core/src/main/java/org/drools/core/SessionConfiguration.java
+++ drools-core/src/main/java/org/drools/core/SessionConfiguration.java
@@ -213,6 +213,8 @@ public class SessionConfiguration
             setTimerJobFactoryType(TimerJobFactoryType.resolveTimerJobFactoryType(StringUtils.isEmpty(value) ? "default" : value));
         } else if ( name.equals( QueryListenerOption.PROPERTY_NAME ) ) {
             setQueryListenerClass( StringUtils.isEmpty( value ) ? QueryListenerOption.STANDARD.getAsString() : value );
+        } else if ( name.equals( BeliefSystemTypeOption.PROPERTY_NAME ) ) {
+            setBeliefSystemType( StringUtils.isEmpty( value ) ? BeliefSystemType.SIMPLE : BeliefSystemType.resolveBeliefSystemType( value ) );
         }
     }
 
@@ -230,6 +232,8 @@ public class SessionConfiguration
             return this.timerJobFactoryType.toExternalForm();
         } else if ( name.equals( QueryListenerOption.PROPERTY_NAME ) ) {
             return this.queryListener.getAsString();
+        } else if ( name.equals( BeliefSystemTypeOption.PROPERTY_NAME ) ) {
+            return this.getBeliefSystemType().getId();
         }
         return null;
     }
@@ -505,6 +509,8 @@ public class SessionConfiguration
             return (T) TimerJobFactoryOption.get( getTimerJobFactoryType().toExternalForm() );
         } else if ( QueryListenerOption.class.equals( option ) ) {
             return (T) this.queryListener;
+        } else if ( BeliefSystemTypeOption.class.equals( option ) ) {
+            return (T) BeliefSystemTypeOption.get( this.getBeliefSystemType().getId() );
         }
         return null;
     }
@@ -535,6 +541,8 @@ public class SessionConfiguration
                                        ((WorkItemHandlerOption) option).getHandler() );
         } else if ( option instanceof QueryListenerOption ) {
             this.queryListener = (QueryListenerOption) option;
+        } else if ( option instanceof BeliefSystemTypeOption ) {
+            this.beliefSystemType = ((BeliefSystemType.resolveBeliefSystemType( ((BeliefSystemTypeOption) option).getBelieSystemType() )));
         }
     }
 
